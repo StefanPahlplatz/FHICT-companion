d0ea35c0c6c58af8be8471cbe57a4dc0b0ca7de3
Stefan Pahlplatz <stefan314@live.nl>
2016-12-15 21:29:34 +0100

    Updated people info layout and hid the keyboard when you search.

--- FHICT-companion/app/src/main/java/s/pahlplatz/fhict_companion/fragments/PeopleFragment.java
+++ FHICT-companion/app/src/main/java/s/pahlplatz/fhict_companion/fragments/PeopleFragment.java
@@ -7,11 +7,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -21,6 +25,7 @@ import java.util.ArrayList;
 
 import s.pahlplatz.fhict_companion.R;
 import s.pahlplatz.fhict_companion.utils.FhictAPI;
+import s.pahlplatz.fhict_companion.utils.Keyboard;
 import s.pahlplatz.fhict_companion.utils.models.Person;
 
 public class PeopleFragment extends Fragment
@@ -28,6 +33,7 @@ public class PeopleFragment extends Fragment
     private static final String TAG = NewsFragment.class.getSimpleName();
 
     private OnPeopleSearchListener mListener;
+    private ImageView logo;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -43,7 +49,22 @@ public class PeopleFragment extends Fragment
 
         getActivity().setTitle("People");
 
+        logo = (ImageView) view.findViewById(R.id.people_logo);
         final EditText et_query = (EditText) view.findViewById(R.id.people_edittext_search);
+
+        et_query.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent)
+            {
+                if (i == EditorInfo.IME_ACTION_SEARCH)
+                {
+                    search(et_query.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
 
         Button btnSearch = (Button) view.findViewById(R.id.people_button_search);
         btnSearch.setOnClickListener(new View.OnClickListener()
@@ -51,11 +72,25 @@ public class PeopleFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-                new LoadResults().execute(et_query.getText().toString());
+                search(et_query.getText().toString());
             }
         });
 
         return view;
+    }
+
+    private boolean search(String query)
+    {
+        if (!query.equals(""))
+        {
+            new LoadResults().execute(query);
+            logo.setVisibility(View.GONE);
+            Keyboard.hide(getActivity());
+            return true;
+        }
+
+        Toast.makeText(getContext(), "Search can't be empty", Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     @Override
